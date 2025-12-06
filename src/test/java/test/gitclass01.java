89467f55b46af876d1c1284693d3a640132626ca
ToyamKhatri <toyam1229@gmail.com>
2025-12-06 16:15:24 -0500

    Done with version 2

--- src/test/java/test/gitclass01.java
+++ src/test/java/test/gitclass01.java
@@ -3,7 +3,9 @@ package test;
 public class gitclass01 {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("This is project #1");
+        //System.out.println("This is project #1");
+        System.out.println("This is version #2");
     }
 }
 //Done with version 1
+//Done with version 2
